03075ef02f3ca05d621e68e16544fb7fbba4cd07
Abhay2004Kumar <abs220502@gmail.com>
2024-12-29 12:23:56 +0530

    added delte-message route and carousel ui

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -70,5 +70,6 @@ const UserSchema: Schema<User> = new Schema({
     })
 
     const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+    
 
     export default UserModel
