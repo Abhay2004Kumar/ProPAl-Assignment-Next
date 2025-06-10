4da178d17d1993fe6589df254c01a6b90ce676a3
Abhay2004Kumar <abs220502@gmail.com>
2025-06-10 14:43:23 +0530

    fixed hydration issue using clientOnly wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import './globals.css'
+import ClientOnly from '@/components/ClientOnly';
 import { ThemeProviderWrapper } from '@/components/theme-provider';
 import { Toaster } from 'react-hot-toast';
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-      <ThemeProviderWrapper>
-        {children}
-        <Toaster position="top-center" />
-        </ThemeProviderWrapper>
+       <body>
+        <ClientOnly>
+          <ThemeProviderWrapper>
+            {children}
+            <Toaster position="top-center" />
+          </ThemeProviderWrapper>
+        </ClientOnly>
       </body>
     </html>
   );
---
+++ src/components/ClientOnly.tsx
@@ -0,0 +1,16 @@
+// components/ClientOnly.tsx
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function ClientOnly({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return <>{children}</>;
+}
